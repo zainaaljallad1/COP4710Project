6833aa3e3ba714c0e7cfd553cdd8aea7829f48ce
zainaaljallad1 <zaina.aljallad@knights.ucf.edu>
2020-04-07 18:47:10 -0400

    update website

---
+++ js/cart-js.js
@@ -0,0 +1,39 @@
+
+var total = 10;
+document.getElementById('totalCost').innerHTML="$"+total;
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('name').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    total += 10;
+    document.getElementById('totalCost').innerHTML="$"+total;
+    document.getElementById('name').value = value;
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('name').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value == 1)
+    {
+      alert("to proceed pls delete");
+      return;
+    }
+    value--;
+    total -= 10;
+    document.getElementById('totalCost').innerHTML="$"+total;
+    document.getElementById('name').value = value;
+}
+function newDiscount()
+{
+    var discountstring = ' ';
+    var arr =["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f",
+              "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+              "x","y","z"];
+    for (var i = 6; i > 0; i--)
+        discountstring += arr[Math.floor(Math.random() * arr.length)];
+
+    alert(discountstring);
+    });
+}
