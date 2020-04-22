31edaf16cdb9dbbae2c71a461aae012c751ed507
austinconnor <connoraustin22@gmail.com>
2020-04-22 13:35:59 -0400

    added php files

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Office Supply Store </title>
+    <link rel="stylesheet" href="css/design.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Bitter&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="title"> Office Supplies </h1>
+
+    <div class="menu">
+      <div>
+      <a href="shopping.html"> Shop </a>
+      </div>
+
+      <div>
+      <a href="discount.html"> Check Discount Status </a>
+      </div>
+
+      <div>
+      <a href="login.html"> Login </a>
+      </div>
+
+      <div>
+      <a href="signup.html"> Sign Up </a>
+      </div>
+
+    </div>
+
+  <div class = "divider"></div>
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
---
+++ signup.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Office Supply Store </title>
+      <link rel="stylesheet" href="css/design.css" type="text/css">
+      <link rel="stylesheet" href="css/design-signup.css" type="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Bitter&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <!-- multistep form -->
+
+  <h1 class="title"> Office Supplies </h1>
+
+  <div class="menu">
+    <div>
+    <a href="shopping.html"> Shop </a>
+    </div>
+
+    <div>
+    <a href="discount.html"> Check Discount Status </a>
+    </div>
+
+    <div>
+    <a href="login.html"> Login </a>
+    </div>
+
+    <div>
+    <a href="signup.html"> Sign Up </a>
+    </div>
+  </div>
+
+<div class = "divider"></div>
+
+  <form id="msform" action="php/submit.php" method="POST">
+    
+    <h2 class="fs-title">Sign Up</h2>
+    <input type="text" name="email" placeholder="Email" />
+    <input type="password" name="pass" placeholder="Password" />
+    <input type="password" name="cpass" placeholder="Confirm Password" />
+    <input type="submit" name="submit" class="next action-button" value="Create Account"/>
+    
+  </form>
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="js/popup.js"></script>
+</html>
